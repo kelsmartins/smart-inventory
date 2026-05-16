c4405eedd12a92a2c62cedd6fa39fc9ee6466122
cnrnival <gutosantoa456@gmail.com>
2026-05-16 13:13:03 -0400

    revert

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,11 @@
-const express = require('express');
+
+
+
+ const jsonServer = require('json-server');
 const fs = require('fs');
 const path = require('path');
 
+<<<<<<< HEAD
 const app = express();
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -370,3 +374,27 @@ app.listen(PORT, () => {
   console.log(`📦 Endpoints: /products , /users`);
 });
 >>>>>>> parent of f7342c4 (fix: corrige rotas /users e /products e atualiza server.js)
+=======
+const productsPath = path.join(dirname, 'products.json');
+const usersPath = path.join(dirname, 'users.json');
+
+// Lê os arquivos separados
+const products = JSON.parse(fs.readFileSync(productsPath, 'utf8'));
+const users = JSON.parse(fs.readFileSync(usersPath, 'utf8'));
+
+// Combina em um único objeto
+const db = { ...products, ...users };
+
+const server = jsonServer.create();
+const router = jsonServer.router(db);
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+// Usa a porta do ambiente (Render) ou 3333 localmente
+const PORT = process.env.PORT || 3333;
+server.listen(PORT, () => {
+ // console.log('JSON Server is running on port' ${PORT});
+});
+>>>>>>> parent of c290f18 (feat: API Express com users.json e products.json)

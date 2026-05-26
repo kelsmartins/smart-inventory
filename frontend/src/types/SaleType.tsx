2b86830706bcc4eb84ec79f21652878de950482a
klss <kelsoglauber11@gmail.com>
2026-05-26 18:58:38 -0400

    Update SaleType.tsx

--- frontend/src/types/SaleType.tsx
+++ frontend/src/types/SaleType.tsx
@@ -1,8 +1,22 @@
 import { ItemCartType } from "./ItemCartType";
 
+// Para itens que vêm do banco de dados
+export type SaleItemType = {
+  id: number;
+  name: string;
+  quantity: number;
+  unit_price: number;
+  sale_id?: number;
+  product_id: number;
+  batch_id?: number;
+};
+
+// Para Venda principal
 export type SaleType = {
   id: number;
   total_price: number;
-  created_at: string;
-  items: ItemCartType[]; 
+  created_at: string; 
+  user_id?: number | null; 
+  
+  items: SaleItemType[]; 
 };

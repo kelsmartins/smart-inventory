c0fdb3f57f17641746aba5cb333898fd0ec532cc
klss <kelsoglauber11@gmail.com>
2026-05-26 22:31:43 -0400

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///instance/validation.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     # === CONFIGURAÇÕES DO JWT E COOKIES ===
